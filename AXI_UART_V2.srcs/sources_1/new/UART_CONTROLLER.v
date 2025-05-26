6f92924a7f637ee8164fb87e368478ccc6552bd8
dhadhireddy-gnaneshwar <116159177+dhadhireddy-gnaneshwar@users.noreply.github.com>
2025-05-26 17:28:23 +0530

    added UART CONTROLLER verilog file need to write logic...

---
+++ AXI_UART_V2.srcs/sources_1/new/UART_CONTROLLER.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.05.2025 17:18:15
+// Design Name: 
+// Module Name: UART_CONTROLLER
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UART_CONTROLLER(
+
+    );
+endmodule
